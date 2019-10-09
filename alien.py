a01c2a86e9cbf565670587e975ee7020de32795a
Hosseinht <42866137+Hosseinht@users.noreply.github.com>
2019-10-09 16:21:47 +0330

    setting method

--- alien.py
+++ alien.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behvaior"""
@@ -9,14 +11,14 @@ class AlienInvasion:
 	def __init__(self):
 		"""Initialize the game, and creat game resources."""
 		pygame.init()
+		self.settings = Settings()
 
-		self.screen  = pygame.display.set_mode((1200, 800))
+		self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
-# self.screen is called surface A surface in
-# Pygame is a part of the screen where a game element can be displayed
 
-		# Set background color
-		self.bg_color = (230, 230, 230)
+	# self.screen is called surface A surface in
+	# Pygame is a part of the screen where a game element can be displayed
+
 
 	def run_game(self):
 		"""Start the main loop of the game"""
@@ -26,13 +28,13 @@ class AlienInvasion:
 				if event.type == pygame.QUIT:
 					sys.exit()
 			# Redraw the screen during each pass through the loop
-			self.screen.fill(self.bg_color)
+			self.screen.fill(self.settings.bg_color)
 
 			# Make the most recently drawn screen visible
 			pygame.display.flip()
+
 
 if __name__ == '__main__':
 	# Make a game instance, and run the game.
 	ai = AlienInvasion()
 	ai.run_game()
-
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+	"""A Class to store all settings for Alien Invasion."""
+
+	def __init__(self):
+		"""Initialie the game's setting."""
+		# Screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
